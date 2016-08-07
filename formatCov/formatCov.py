52b8dc52cc33895512c38e032c5533d4d84feb04
Eric Jiang <ericjiang97@gmail.com>
2016-08-07 21:46:10 +1000

    renamed filename function for compability

--- formatCov/formatCov.py
+++ formatCov/formatCov.py
@@ -2,7 +2,7 @@ import os
 fformat = str(input('Enter format type (e.g. .jpeg): '))
 
 for filename in os.listdir("."):
-        if filename.startswith('smartrename.py') is False:
+        if filename.startswith('formatCov.py') is False: #converts all the file except for the Python File
             os.rename(filename,filename+fformat)
     
     
